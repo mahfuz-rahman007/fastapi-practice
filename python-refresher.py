19c60089f1979c60c2c5c6afc3f34fbd31d84b30
mahfuz-rahman007 <mahfujkhan017@gmail.com>
2025-04-27 22:56:22 +0600

    Work with User input

--- python-refresher.py
+++ python-refresher.py
@@ -10,3 +10,9 @@
 first_name = input("Enter Your Name: ")
 age = input("How old are you? ")
 print(f"Hi {first_name}, You are {age} years old")
+
+first_name = input("Enter Your Name: ")
+days_until_bd = input("How Many Days Until Your Birthday? ")
+weeks_remaining = int(int(days_until_bd)/7)
+remaining_days = int(days_until_bd)%7
+print(f"Hi {first_name}, You have {weeks_remaining} Weeks and {remaining_days} Days before your birthday. Happy Birthday")
