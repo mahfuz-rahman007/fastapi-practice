06305a28608e0f85e6a44b4c6e7bcd0a726ec5ec
mahfuz-rahman007 <mahfujkhan017@gmail.com>
2025-04-27 22:48:12 +0600

    user input practice

--- python-refresher.py
+++ python-refresher.py
@@ -1,8 +1,12 @@
 # String Formatting
 
-first_name = "Mahfuz"
-print(f"Hi {first_name}")
+# first_name = "Mahfuz"
+# print(f"Hi {first_name}")
+# sentence = "Hi This is {} {}"
+# last_name = "Rahman"
+# print(sentence.format(first_name,last_name))
 
-sentence = "Hi This is {} {}"
-last_name = "Rahman"
-print(sentence.format(first_name,last_name))
+#User Input
+first_name = input("Enter Your Name: ")
+age = input("How old are you? ")
+print(f"Hi {first_name}, You are {age} years old")
